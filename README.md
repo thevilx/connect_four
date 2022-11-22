3f08c0d0a6dab890dd2d86a03560181e74258776
thevil <amin.mir1380@gmail.com>
2022-11-23 02:18:16 +0330

    add dynamic rows & col system

---
+++ README.md
@@ -0,0 +1,6 @@
+# Connect Four Game Written In JS
+This is a simple connect four game written in pure JS.
+
+<p align="left"><a href="https://laravel.com" target="_blank"><img src="https://i.ibb.co/fH2Lpqs/connet-four.png" alt="Connect-Four" ></a></p>
+
+
--- app.js
+++ app.js
@@ -1,53 +1,83 @@
+let row = 12;
+let col = 7;
+
 const main = document.getElementById('main');
-const grid = document.createElement('div');
-grid.classList.add('grid');
-main.appendChild(grid);
-
-const col = 7;
-const row = 7;
-
-for (let i = 0; i < 49; i++) {
-    let element = document.createElement('div');
-    grid.appendChild(element);
-}
-
-const squares = document.querySelectorAll('.grid div');
+const remakeGameButton = document.getElementById('remake_game');
+let squares;
 const results = document.querySelector('#result');
 const displayCurrentPlayer = document.querySelector('#current-player');
+const player_color = document.querySelector('#player_color');
 let currentPlayer = 1;
+displayCurrentPlayer.innerHTML = currentPlayer;
 
 
-for (let i = 0; i < squares.length; i++) {
-    squares[i].onclick = () => {
-        for (let j = 0; j < col; j++) {
+drawMap();
 
-            let pos = (col * row) - j * col - (col - (i % 7));
-            // console.log(pos);
-            if (!squares[pos].classList.contains('taken')) {
-                plantPlayer(currentPlayer, pos);
-                currentPlayer = currentPlayer == 1 ? 2 : 1;
 
-                break
+function drawMap(){
+    let old_grid = document.querySelector('.grid');
+    if(old_grid)
+        old_grid.parentNode.removeChild(old_grid);
 
+    const grid = document.createElement('div');
+    grid.style.width = row * 40 + "px"; // create size based on row & col size
+    grid.style.height = col * 40 + "px";
+    grid.classList.add('grid');
+    main.appendChild(grid);
+
+    // create elements in rows col size
+
+    for (let i = 0; i < col * row; i++) {
+        let element = document.createElement('div');
+        grid.appendChild(element);
+    }
+
+    squares = document.querySelectorAll('.grid div');
+
+    for (let i = 0; i < squares.length; i++) {
+
+        squares[i].onclick = () => {
+            for (let j = 0 ; j < col; j++) {
+
+                // let pos = (col * row) - j * row - (col - (i % row));
+                let pos = (col * row) - j * row - (row - (i % row));
+                // console.log((col * row) - j * row , (i % row) , pos);
+                // let pos = i - j * row;
+                // console.log(pos);
+                if (!squares[pos].classList.contains('taken')) {
+        
+                    plantPlayer( pos);
+
+                    break
+
+                }
             }
         }
+
     }
+
 }
 
-function plantPlayer(player_id, element_id) {
+function plantPlayer(element_id) {
+    
     squares[element_id].classList.add('taken')
-    squares[element_id].setAttribute('player_own', player_id)
-    squares[element_id].classList.add(player_id == 1 ? 'player-one' : 'player-two')
-    displayCurrentPlayer.innerHTML = player_id;
+    squares[element_id].setAttribute('player_own', currentPlayer)
+    squares[element_id].classList.add(currentPlayer == 1 ? 'player-one' : 'player-two')
+    
+    currentPlayer = currentPlayer == 1 ? 2 : 1; // toggle player
+    updatePlayerDispay();
     checkWin(element_id);
 }
 
 function checkWin(last_pos_entered) {
-    let col_number = Math.floor(last_pos_entered / col);
+    let col_number = Math.floor(last_pos_entered / row);
     let row_number = last_pos_entered % row;
+    // console.log(col_number, row_number , "maadad");
+    // console.log(col_number * col, col_number * col + col , 1 , "satasrt")
+    // console.log(col_number , row_number , "Sadasdasdasd")
+    checkLineMatch(col_number * row, col_number * row + row , 1); // check line match in horizental order
+    checkLineMatch(row_number, col * row, row); // check line match in vertical order
 
-    checkLineMatch(col_number * col, col_number * col + col, 1);
-    checkLineMatch(row_number, col * row, col);
     checkDiagonalRTL(row_number, col_number);
     checkDiagonalLTR(row_number , col_number);
 }
@@ -62,14 +92,16 @@ function checkDiagonalRTL(row_number, col_number) {
     } else {
         start_col_number = col_number - row_number;
         start_row_number = 0;
-    }
+    }   
+
+    // console.log(start_row_number , start_col_number);
 
     let count_of_match = 0;
-    let starting = squares[start_col_number * col + start_row_number].getAttribute('player_own');
+    let starting = squares[start_col_number * row + start_row_number].getAttribute('player_own');
 
     while (true) {
 
-        element = squares[start_col_number * col + start_row_number].getAttribute('player_own');
+        element = squares[start_col_number * row + start_row_number].getAttribute('player_own');
 
         if (starting == element && element !== null) {
             ++count_of_match;
@@ -77,7 +109,7 @@ function checkDiagonalRTL(row_number, col_number) {
             starting = element;
             count_of_match = 1;
         }
-
+       
         if (count_of_match >= 4) {
             match = true;
         }
@@ -92,10 +124,7 @@ function checkDiagonalRTL(row_number, col_number) {
     }
 
 
-    if (match) {
-        alert("Player " + currentPlayer + " Win ! Congrates");
-        reset()
-    }
+    if (match) gameOver();
 }
 
 function checkDiagonalLTR(row_number, col_number) {
@@ -106,17 +135,17 @@ function checkDiagonalLTR(row_number, col_number) {
         start_row_number = row_number + col_number;
         start_col_number = 0;
     } else {
-        // console.log((row_number + col_number) - (col - 1));
-        start_row_number = col - 1;
-        start_col_number = (row_number + col_number) - (col - 1);
+        start_row_number = row - 1;
+        start_col_number = (row_number + col_number) - (row - 1);
     }
 
+
     let count_of_match = 0;
-    let starting = squares[start_col_number * col + start_row_number].getAttribute('player_own');
+    let starting = squares[start_col_number * row + start_row_number].getAttribute('player_own');
 
     while (true) {
 
-        element = squares[start_col_number * col + start_row_number].getAttribute('player_own');
+        element = squares[start_col_number * row + start_row_number].getAttribute('player_own');
 
         if (starting == element && element !== null) {
             ++count_of_match;
@@ -125,7 +154,6 @@ function checkDiagonalLTR(row_number, col_number) {
             count_of_match = 1;
         }
 
-        // console.log(count_of_match);
         if (count_of_match >= 4) {
             match = true;
         }
@@ -141,18 +169,17 @@ function checkDiagonalLTR(row_number, col_number) {
     }
 
 
-    if (match) {
-        alert("Player " + currentPlayer + " Win ! Congrates");
-        reset()
-    }
+    if (match) gameOver();
+
 }
 
 function checkLineMatch(start, end, jump) {
     let starting = squares[start].getAttribute('player_own');
     let match = false;
     let count_of_match = 0;
-
+    console.log(squares);
     for (i = start; i < end; i += jump) {
+        console.log(i);
         let element = squares[i].getAttribute('player_own');
 
         if (starting == element && element !== null) {
@@ -163,19 +190,35 @@ function checkLineMatch(start, end, jump) {
             count_of_match = 1;
         }
 
+        // console.log(count_of_match);
+
         if (count_of_match >= 4) {
             match = true;
         }
     }
 
-    if (match) {
-        alert("Player " + currentPlayer + " Win ! Congrates");
-        reset()
-    }
+    if (match) gameOver();
+}
+
+function gameOver(){
+    alert("Player " + currentPlayer + " Win ! Congrates");
+    reset()
 }
 
 function reset() {
     // currentPlayer = 1;
-    window.location.reload()
+    drawMap();
 }
 
+function updatePlayerDispay(){
+    displayCurrentPlayer.innerHTML = currentPlayer;
+    player_color.style.color = currentPlayer == 1 ? 'red' : 'blue';
+}
+
+function remakeGame(){
+    row = parseInt(document.getElementById('row_size').value);
+    col = parseInt(document.getElementById('col_size').value);
+    currentPlayer = 1;
+    updatePlayerDispay();
+    drawMap();
+}
